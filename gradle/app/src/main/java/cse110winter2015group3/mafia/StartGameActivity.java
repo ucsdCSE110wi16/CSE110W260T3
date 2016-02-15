927b5296957f5aa73cb6ace996ee9f2c11c5ba61
lucngo <l8ngo@ucsd.edu>
2016-02-14 18:01:56 -0800

    update

---
+++ gradle/app/src/main/java/cse110winter2015group3/mafia/StartGameActivity.java
@@ -0,0 +1,16 @@
+package cse110winter2015group3.mafia;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class StartGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_game);
+    }
+
+}
